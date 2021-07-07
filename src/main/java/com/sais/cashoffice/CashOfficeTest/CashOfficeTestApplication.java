f2cba181ad4af1ff1357c7113b38738e2bea8d5b
sagineni <sagineni@techbulls.co.in>
2021-07-07 13:27:47 +0530

    testing reseting with hard copy

--- src/main/java/com/sais/cashoffice/CashOfficeTest/CashOfficeTestApplication.java
+++ src/main/java/com/sais/cashoffice/CashOfficeTest/CashOfficeTestApplication.java
@@ -16,6 +16,7 @@ public class CashOfficeTestApplication {
 	private static final Logger LOGGER = LogManager.getLogger(CashOfficeTestApplication.class);
 
 	public static void main(String[] args) {
+		// testing reset
 		SpringApplication.run(CashOfficeTestApplication.class, args);		
 	}
 	
